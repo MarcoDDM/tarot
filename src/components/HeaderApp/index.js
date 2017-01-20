544e21027c181dda027a50613bfd5d906eb82b42
= <thiagomunhozmedeiros@gmail.com>
2017-01-19 23:30:22 -0200

    Componente HeaderApp, Imagens dos Arcanos

---
+++ src/components/HeaderApp/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './index.sass';
+
+function HeaderApp(){
+  return (
+    <div className='header-app'>
+      <h1>MyTarot</h1>
+      <button>Mentalize sua questão e clique aqui</button>
+    </div>
+  )
+}
+
+export default HeaderApp;
---
+++ src/core/arcanums.js
@@ -0,0 +1,26 @@
+module.exports = {
+	major: {
+		0: { arcanum: 'O Louco' },
+		1: { arcanum: 'O Mago' },
+		2: { arcanum: 'A Sarcedotiza' },
+		3: { arcanum: 'A Imperatriz' },
+		4: { arcanum: 'O Imperador' },
+		5: { arcanum: 'O Papa' },
+		6: { arcanum: 'Os Enamorados' },
+		7: { arcanum: 'O Carro' },
+		8: { arcanum: 'A Força' },
+		9: { arcanum: 'O Eremita' },
+		10: { arcanum: 'A Roda da Fortuna' },
+		11: { arcanum: 'A Justiça' },
+		12: { arcanum: 'O Enforcado' },
+		13: { arcanum: 'A Morte' },
+		14: { arcanum: 'A Temperança' },
+		15: { arcanum: 'O Diabo' },
+		16: { arcanum: 'A Torre' },
+		17: { arcanum: 'A Estrela' },
+		18: { arcanum: 'A Lua' },
+		19: { arcanum: 'O Sol' },
+		20: { arcanum: 'O Julgamento' },
+		21: { arcanum: 'O Mundo' }
+	}
+};
