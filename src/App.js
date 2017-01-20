19aa8a6be3ac2893cb7dcffe7437773a0820363d
= <thiagomunhozmedeiros@gmail.com>
2017-01-19 23:29:22 -0200

    Config inicial - Create React App

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import HeaderApp from './components/HeaderApp';
+
+
+class App extends Component {
+  render() {
+    return (
+      <HeaderApp />
+      
+    );
+  }
+}
+
+export default App;
