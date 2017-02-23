431a132d9f435e7066486b8e70b032ba2ec98441
= <thiagomunhozmedeiros@gmail.com>
2017-02-23 00:07:41 -0300

    Added some card descriptions

--- src/helpers/arcanums.js
+++ src/helpers/arcanums.js
@@ -1,26 +1,92 @@
 module.exports = {
 	major: {
-		0: { arcanum: 'O Louco' },
-		1: { arcanum: 'O Mago' },
-		2: { arcanum: 'A Sarcedotiza' },
-		3: { arcanum: 'A Imperatriz' },
-		4: { arcanum: 'O Imperador' },
-		5: { arcanum: 'O Papa' },
-		6: { arcanum: 'Os Enamorados' },
-		7: { arcanum: 'O Carro' },
-		8: { arcanum: 'A Força' },
-		9: { arcanum: 'O Eremita' },
-		10: { arcanum: 'A Roda da Fortuna' },
-		11: { arcanum: 'A Justiça' },
-		12: { arcanum: 'O Enforcado' },
-		13: { arcanum: 'A Morte' },
-		14: { arcanum: 'A Temperança' },
-		15: { arcanum: 'O Diabo' },
-		16: { arcanum: 'A Torre' },
-		17: { arcanum: 'A Estrela' },
-		18: { arcanum: 'A Lua' },
-		19: { arcanum: 'O Sol' },
-		20: { arcanum: 'O Julgamento' },
-		21: { arcanum: 'O Mundo' }
+		0: {
+      name: 'O Louco',
+      description: 'Esta carta simboliza o início de algo novo. É tempo de começar ou recomeçar. O Louco traz a novidade, a insegurança dos novos começos, a incerteza porque ainda não sabemos o que vai acontecer, mas traz em si a promessa de que tudo pode acontecer. Pode indicar que nos encontramos num ponto da nossa vida em que estamos prontos para embarcar numa nova etapa e descobrir novas aventuras. Se sua questão é a respeito de amor, o Louco pode indicar a chegada de um novo amor ou a necessidade de ter maior liberdade. Pode, também, indicar que a pessoa ainda não sabe bem aquilo que quer. Esta é uma fase de descoberta, em que deve deixar os sentimentos fluírem sem querer impor-lhes uma ordem fixa. Se sua questão é a respeito de saúde esta carta pode indicar instabilidade, pode também alertar para a necessidade de ser mais cuidadoso com a sua saúde. Pode ser um alerta para os excessos ou para uma atitude negligente. Se sua questão é a respeito de trabalho indica uma possível viagem, um novo trabalho, uma nova etapa, ou alerta para a necessidade de abraçar novos desafios e experimentar domínios onde antes não arriscou aventurar-se. É uma carta que não traz estabilidade mas que favorece os riscos, as viagens e as novidades.',
+    },
+		1: {
+      name: 'O Mago',
+      description: 'O Mago é o número Um, o número da criação e da individualidade. Seu poder é a transformação por meio da vontade. O Mago nos remete para o ilusionista, aquele que, com habilidade, transforma as coisas em que toca. Esta carta representa a habilidade para lidar com as situações, com as circunstâncias e com todos os elementos que nos rodeiam. É uma carta positiva, que embora peça prudência e cuidado indica que temos boas possibilidades de conseguir superar de forma positiva a situação, pois temos na nossa mão tudo aquilo de que precisamos para sermos bem sucedidos, tendo também a competência e a habilidade mental necessárias para alcançarmos os objetivos. Agora é o momento de atuar, dado que você tem todo o poder do Mago, converta seus desejos em objetivos, seus pensamentos em ações, suas metas em resultados. E se recentemente fracassou, agora é o momento de transformar este fracasso em êxito, assim como o Mago transforma o fogo em água. Por fim, a mensagem do Mago é simples: sua vida está sob o seu controle, sua vida é o que você quer que seja, sua vida é como você a faz.',
+    },
+		2: {
+      name: 'A Sarcedotiza',
+      description: 'Esta carta nos mostra uma mulher sábia, uma sacedortisa antiga, que detém o conhecimento e a intuição necessárias para saber agir da forma mais adequada. Ela representa a nossa voz interior e o nosso inconsciente. Esta carta representa a serenidade, o estudo e o mistério. Ela indica que você está preparado(a) para resolver as questões que se apresentam e sabe fazê-lo com sabedoria, maestria e inteligência. Mas você deve prestar muita atenção à sua intuição para saber compreender os sinais que esta lhe envia. A sua voz interior é a sua mais sábia conselheira. É uma carta que incita à prudência, e te convida a esperar antes de agir, não se precipite nem aja por impulso, procure primeiro saber muito bem aquilo que está à sua volta e com o que pode contar. Ela indica que deve usar a sua sensibilidade para conhecer melhor as pessoas e compreender as suas motivações. Mantenha os seus planos em segredo, até que as circunstâncias lhe sejam mais oportunas. É uma carta também associada ao esoterismo, ao estudo da sabedoria oculta e ao desenvolvimento da intuição que todos possuímos, mas a que nem sempre sabemos dar o devido valor.',
+    },
+		3: {
+      name: 'A Imperatriz',
+      description: 'A Imperatriz geralmente tem significância positiva. Intimamente ligada ao corpo físico e mundo material, dela provém o prazer nos sentidos e a abundância da vida em suas diferentes formas. A Imperatriz nos indica um tempo de boas colheitas, ou seja, você está colhendo as sementes que plantou. Esta carta simboliza a riqueza e a recompensa pelos esforços que foram feitos. Ela simboliza, também, o esforço que é necessário fazer para superar as provas, e indica a vitória através da sabedoria e do bom uso das palavras, da diplomacia e da sensibilidade. Acima de tudo, A Imperatriz representa o arquétipo da Mãe, e com frequência pode pressagiar a concepção e o nascimento de uma criança. Por representar o arquétipo da Mãe, certamente você se encontra em um ambiente seguro e isento de perigos. Aproveite este momento de abundância, criatividade e produtividade para plantar novas sementes, que lhe renderão uma colheita generosa no futuro. Faça um esforço para abrir-se ao seu amor incondicional.',
+    },
+		4: {
+      name: 'O Imperador',      
+      description: '',
+    },
+		5: {
+      name: 'O Papa',      
+      description: '',
+    },
+		6: {
+      name: 'Os Enamorados',      
+      description: '',
+    },
+		7: {
+      name: 'O Carro',      
+      description: '',
+    },
+		8: {
+      name: 'A Força',      
+      description: '',
+    },
+		9: {
+      name: 'O Eremita',      
+      description: '',
+    },
+		10: {
+      name: 'A Roda da Fortuna',      
+      description: '',
+    },
+		11: {
+      name: 'A Justiça',      
+      description: '',
+    },
+		12: {
+      name: 'O Enforcado',      
+      description: '',
+    },
+		13: {
+      name: 'A Morte',      
+      description: '',
+    },
+		14: {
+      name: 'A Temperança',      
+      description: '',
+    },
+		15: {
+      name: 'O Diabo',      
+      description: '',
+    },
+		16: {
+      name: 'A Torre',      
+      description: '',
+    },
+		17: {
+      name: 'A Estrela',      
+      description: '',
+    },
+		18: {
+      name: 'A Lua',      
+      description: '',
+    },
+		19: {
+      name: 'O Sol',      
+      description: '',
+    },
+		20: {
+      name: 'O Julgamento',      
+      description: '',
+    },
+		21: {
+      name: 'O Mundo',      
+      description: '',
+    }
 	}
 }
