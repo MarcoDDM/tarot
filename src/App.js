189c71196916341eaff6bf3d285701928a772a73
= <thiagomunhozmedeiros@gmail.com>
2017-03-03 00:09:41 -0300

    installing the bootstrap-css-only

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import HeaderApp from './components/HeaderApp/HeaderApp'
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
+import 'bootstrap-css-only'
 import './assets/css/base.sass'
 import './assets/css/animations.sass'
 
 const App = ({ children, location }) => (
-  <main>
+  <main className="container">
     <HeaderApp/>
     <ReactCSSTransitionGroup
       transitionName="page-transition"
