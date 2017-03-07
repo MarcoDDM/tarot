0f99024cb93d3ddf654a6e803b6729db063fc4f8
= <thiagomunhozmedeiros@gmail.com>
2017-03-06 23:03:59 -0300

    Switching react-router from 'browserHistory' to 'hasHistory' to works on gh-pages

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import { Router, Route, IndexRoute, browserHistory } from 'react-router'
+import { Router, Route, IndexRoute, hashHistory } from 'react-router'
 import App from './containers/App/App'
 import Index from './containers/Index/Index'
 import TarotTable from './containers/TarotTable/TarotTable'
@@ -10,7 +10,7 @@ import store from './store'
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router history={browserHistory}>
+    <Router history={hashHistory}>
       <Route path="/" component={App}>
         <IndexRoute component={Index}/>
         <Route path="tarot-table" component={TarotTable} />
