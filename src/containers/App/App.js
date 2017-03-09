39e930f2c9ebbc12986dfa546417b57b4f819432
= <thiagomunhozmedeiros@gmail.com>
2017-03-08 22:24:15 -0300

    Installing and configuring the material-ui

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,22 +1,32 @@
 import React from 'react'
 import HeaderApp from '../../components/HeaderApp/HeaderApp'
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import 'bootstrap-css-only'
 import '../../assets/css/base.sass'
 import '../../assets/css/animations.sass'
 
+injectTapEventPlugin();
+
 const App = ({ children, location }) => (
-  <main className="container">
-    <HeaderApp/>
-    <ReactCSSTransitionGroup
-      transitionName="page-transition"
-      transitionAppear={true}
-      transitionAppearTimeout={50}
-      transitionLeave={false}
-      transitionEnterTimeout={300}>
-      {React.cloneElement(children, { key: location.pathname })}
-    </ReactCSSTransitionGroup>
-  </main>
+  <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+    <main className="main">
+      <HeaderApp/>
+      <div className="container">
+        <ReactCSSTransitionGroup
+          transitionName="page-transition"
+          transitionAppear={true}
+          transitionAppearTimeout={50}
+          transitionLeave={false}
+          transitionEnterTimeout={300}>
+          {React.cloneElement(children, { key: location.pathname })}
+        </ReactCSSTransitionGroup>
+      </div>
+    </main>
+  </MuiThemeProvider>
 )
 
 export default App
