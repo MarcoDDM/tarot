94ac6e16a82660a0da360d6534fabee8df87f8fc
= <thiagomunhozmedeiros@gmail.com>
2017-03-28 23:28:18 -0300

    Put the persistedState in redux store

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import { hashHistory } from 'react-router'
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
 import { syncHistoryWithStore, routerReducer, routerMiddleware } from 'react-router-redux'
+import { loadState } from '../helpers'
 
 import thunk from 'redux-thunk'
 import createLogger from 'redux-logger'
@@ -8,10 +9,12 @@ import promise from 'redux-promise'
 import reducers from '../reducers'
 
 const logger = createLogger()
+const persistedState = () => loadState()
 
 export const store = createStore(
   combineReducers({
     ...reducers,
+    persistedState: persistedState,
     routing: routerReducer,
   }),
   compose(
